4e3281cf6b78aca273a06f251966bba94bb411c0
Ashley B <ashleybi2005@gmail.com>
2021-07-15 12:12:00 -0400

    Added UI

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-//selections are ints that correspond to the indexes in options
-const options = ["rock", "paper", "scissors"];
+//variables
+const options = ["rock", "paper", "scissors"];  //selections are ints that correspond to the indexes in options
+const res = document.querySelector("#results");
+const score = document.querySelector("#score");
+
 var playerScore = 0;
 var computerScore = 0;
 
-function computerPlay() {
+//functions
+function computerPlay() {   //computer choice
     choice = Math.floor(Math.random() * 3)
     return choice;
 };
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {    //check matchup
     if (playerSelection - 1 === computerSelection || playerSelection === 0 && computerSelection === 2) {
         playerScore += 1;
         return `You win! ${options[playerSelection]} beats ${options[computerSelection]}!`
@@ -20,26 +24,27 @@ function playRound(playerSelection, computerSelection) {
     };
 };
 
-function game() {
-    var validInputs = 0;
-    while (validInputs < 5) {
-        var playerChoice = prompt("Pick your weapon!"); //returns string
-        playerChoice = playerChoice.toLowerCase();
-        if (options.includes(playerChoice)) {       //check if input is valid
-            playerChoice = options.indexOf(playerChoice.toLowerCase()); //get 0, 1, or 2
-            console.log(playRound(playerChoice, computerPlay()));
-            validInputs++;
-        } else {
-            console.log("Invalid input!")
-        };
-    };
-    if (playerScore > computerScore) {
-        console.log("You Win!")
-    } else if (playerScore < computerScore) {
-        console.log("You Lose!")
-    } else {
-        console.log("It's a Draw!")
+function getResult() {
+    console.log(playerScore)
+    if (playerScore === 5) {
+        res.textContent = "You Win!";
+        playerScore = 0;
+    } else if (computerScore === 5) {
+        res.textContent = "You Lose!";
+        playerScore = 0;
     };
 };
 
-game();
+function game(playerChoice) {
+    res.textContent = (playRound(playerChoice, computerPlay()));
+    score.textContent = (`Your Score: ${playerScore} - Computer Score: ${computerScore}`)
+    getResult()
+};
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    game(parseInt(button.id))
+  });
+});
